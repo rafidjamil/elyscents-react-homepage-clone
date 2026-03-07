4445e3e12971ae332788d47db4d28a899ba0356a
Rafid Jamil <rafidjamil001@gmail.com>
2026-03-07 07:06:19 -0800

    Fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: process.env.VITE_BASE_URL || '/elyscents-react-homepage-clone',
+    base: process.env.VITE_BASE_PATH || '/elyscents-react-homepage-clone',
 })
