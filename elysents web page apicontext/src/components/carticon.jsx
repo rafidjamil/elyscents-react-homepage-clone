45363cf3a1e25c350f99424ccca7357a3435574d
Rafid Jamil <rafidjamil001@gmail.com>
2026-03-06 17:42:48 -0800

    Delete elysents web page apicontext directory

--- elysents web page apicontext/src/components/carticon.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Carticon = () => {
-  return (
-     <Nav.Link as={Link} to="/" > <HiOutlineShoppingBag onClick={handleShow} className="icon" /></Nav.Link>
-  )
-}
-
-export default Carticon
